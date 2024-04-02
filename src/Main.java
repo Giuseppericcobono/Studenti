ba8019a81d5e8f0e8a5e4967854e0366806399dc
Giuseppericcobono <giuseppe.riccobono@hotmail.com>
2024-04-02 17:05:23 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Studente student1 = new Studente ("Giuseppe", "Riccobono", 145);
+        System.out.println(student1);
+    }
+}
